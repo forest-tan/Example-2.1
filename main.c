11c5262e5961366d4e9d3cd7810ebab0eaf22115
Forest Tan <forest.tan@singaporetech.edu.sg>
2020-09-13 04:17:52 +0000

    v

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 /* Example 2.1 My First Program */
+/* version 2 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void)
 {
-    printf("Hello world!\n");
+    printf("Hello world v2!\n");
     return 0;
 }
